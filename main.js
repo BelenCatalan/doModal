8723203570f73d85438e4dda7ab0f1c970741e1e
BelenCatalan <labdelante@gmail.com>
2020-11-11 20:23:10 +0100

    add html code, styles, and one way to do the modal in js

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 'use strict';
+
+//Primera forma
+if(document.querySelector('.btn__modal')){
+let modal = document.querySelector('.modal__container');
+let btn = document.querySelector('.btn__modal');
+let span = document.querySelector('.modal__close');
+let body= document.getElementsByTagName('.body')[0];
+
+btn.onclick = function(){
+  modal.style.display = "block";
+  body.style.position ="static";
+  body.style.height="100%";
+  body.style.overflow="hidden";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+
+  body.style.position = "inherit";
+  body.style.height = "auto";
+  body.style.overflow = "visible";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+
+    body.style.position = "inherit";
+    body.style.height = "auto";
+    body.style.overflow = "visible";
+  }
+}
+
+}
+
+//Segunda forma
